11f2c7482c5ce5da30da4e8917d5b7fff83b30d9
Karthik Chidambaram <karthik@Phoenix.(none)>
2015-04-28 12:00:09 +0530

    Added support for providing credentials as arguments

--- lib/blitz/command/api.rb
+++ lib/blitz/command/api.rb
@@ -2,38 +2,42 @@ class Blitz
 class Command
 class API < Command # :nodoc:
     attr_accessor :credentials
-        
+
     def cmd_init argv
         FileUtils.rm credentials_file rescue nil
+        p "*&&&&&&&&&*"
+        p argv
+        p "*&&&&&&&&&*"
+        @argv = argv
         API.client
-        
+
         msg "You are now ready to blitz!"
         msg "Try blitz help to learn more about the commands."
     end
-    
+
     def client
         get_credentials
         Blitz::Client.new(user, password, host)
     end
-    
+
     def user
         get_credentials
         @credentials[0]
     end
-    
+
     def password
         get_credentials
         @credentials[1]
     end
-    
+
     def host
         ENV['BLITZ_HOST'] || 'www.blitz.io'
     end
-    
+
     def credentials_file
         ENV['HOME'] + '/.blitz/credentials'
     end
-    
+
     def get_credentials
         return if @credentials
         unless @credentials = read_credentials
@@ -42,30 +46,35 @@ class API < Command # :nodoc:
         end
         @credentials
     end
-    
+
     def read_credentials
-        File.exists?(credentials_file) and File.read(credentials_file).split("\n")        
+        File.exists?(credentials_file) and File.read(credentials_file).split("\n")
     end
-    
+
     def ask_for_credentials
-        msg "Enter your blitz credentials. You can find this in #{green('Settings/API Key')}."
-        print "User-ID: "
-        user = ask
-        print "API-Key: "
-        apik = ask
+        if @argv.blank?
+          msg "Enter your blitz credentials. You can find this in #{green('Settings/API Key')}."
+          print "User-ID: "
+          user = ask
+          print "API-Key: "
+          apik = ask
+        else
+          user = argv[0]
+          apik = argv[1]
+        end
         apik2 = Blitz::Client.new(user, apik, host).login['api_key']
         if not apik2
             error "Authentication failed"
             exit 1
         end
-        
+
         [ user, apik2 ]
     end
-    
+
     def save_credentials
         write_credentials
     end
-    
+
     def write_credentials
         FileUtils.mkdir_p(File.dirname(credentials_file))
         File.open(credentials_file, 'w') do |f|
@@ -73,20 +82,20 @@ class API < Command # :nodoc:
         end
         set_credentials_permissions
     end
-    
+
     def set_credentials_permissions
         FileUtils.chmod 0700, File.dirname(credentials_file)
-        FileUtils.chmod 0600, credentials_file        
+        FileUtils.chmod 0600, credentials_file
     end
-    
+
     def self.instance
         @instance ||= API.new
     end
-    
+
     def self.client
         self.instance.client
     end
-    
+
     private
     def initialize
     end
